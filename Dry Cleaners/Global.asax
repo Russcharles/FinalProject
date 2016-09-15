8bea504549559efc0d0264f56a1fc6253ae4bf68
Russell Brooks <Russellc.brooks@gmail.com>
2016-09-15 12:19:13 -0500

    Add project files.

---
+++ Dry Cleaners/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dry_Cleaners.MvcApplication" Language="C#" %>
---
+++ Dry Cleaners/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dry_Cleaners.Startup))]
+namespace Dry_Cleaners
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
